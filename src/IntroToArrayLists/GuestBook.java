dd9f1f86268729d9d7e7be584f8a946890500f1d
DavidAndBlake <league@WorthyMini-5.attlocal.net>
2017-10-19 16:58:46 -0700

    Set up an action listener for the name buttons

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -2,18 +2,23 @@ package IntroToArrayLists;
 
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GuestBook {
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other
 	// button reads "View Names".
 	JFrame frame = new JFrame();
 	JButton buttonToAddName = new JButton();
 	JButton buttonToViewName = new JButton();
 	JPanel panel = new JPanel();
+	ArrayList<String> namesList = new ArrayList<String>();
 public static void main(String[]args)
 {
 	GuestBook g = new GuestBook();
@@ -28,15 +33,29 @@ public static void main(String[]args)
 		buttonToViewName.setText("View Name");
 		panel.setLayout(new GridLayout());
 		frame.pack();
-		System.out.println("This is getting accessed");
+		buttonToAddName.addActionListener(this);
+		buttonToViewName.addActionListener(this);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		
 	}
-	// When the add name button is clicked, display an input dialog that asks the
-	// user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// When the "View Names" button is clicked, display a
 	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		if (e.getSource() == buttonToAddName) {
+			String name = JOptionPane.showInputDialog("Enter a name");
+			namesList.add(name);
+		}
+		if (e.getSource() == buttonToViewName) {
+			System.out.println(namesList);
+		}
+		
+	}
 }
