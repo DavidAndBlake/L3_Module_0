9c590cc28cb68232848d77478de7317b1fd37b2a
Garrett-Thomas <league@WTS5.attlocal.net>
2017-11-16 16:57:56 -0800

    made some progress

--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -1,6 +1,5 @@
 package IntroToHashMaps;
 
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -10,7 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class LogSearch implements ActionListener{
+public class LogSearch implements ActionListener {
 	HashMap<Integer, String> hashbrown = new HashMap<Integer, String>();
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
@@ -18,61 +17,59 @@ public class LogSearch implements ActionListener{
 	JButton button2 = new JButton();
 	JButton button3 = new JButton();
 	int IDinteger;
+	int widthOfScreen = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+	int heightOfScreen = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
 
 	public static void main(String[] args) {
 		LogSearch Log = new LogSearch();
 		Log.gui();
 	}
-  public void gui(){
-	  frame.setVisible(true);
-	  frame.add(button1);
-	  frame.add(button2);
-	  frame.add(button3);
-	 
-	  button1.addActionListener((ActionListener) this);
-	  button2.addActionListener((ActionListener) this);
-	  button3.addActionListener((ActionListener) this);
-	  
-	
-		
+
+	public void gui() {
+		frame.setLocation(widthOfScreen / 2, heightOfScreen / 2);
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		button1.addActionListener((ActionListener) this);
+		button2.addActionListener((ActionListener) this);
+		button3.addActionListener((ActionListener) this);
+		frame.pack();
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	}
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	  if (e.getSource() == button1) {
-		  String enterID = JOptionPane.showInputDialog("Please enter an ID number");
-		  IDinteger = Integer.parseInt(enterID);
-		  String enterName = JOptionPane.showInputDialog("Enter a name");
-		  hashbrown.put(IDinteger, enterName);
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == button1) {
+			String enterID = JOptionPane.showInputDialog("Please enter an ID number");
+			IDinteger = Integer.parseInt(enterID);
+			String enterName = JOptionPane.showInputDialog("Enter a name");
+			hashbrown.put(IDinteger, enterName);
 		}
-	
-}
-  
-	/* 
-	 * Crate a HashMap of Integers for the keys and Strings for the values.
-	 * Create a GUI with three buttons. 
-	 * Button 1: Add Entry
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				After an ID is entered, use another input dialog to ask the user to enter a name.
-	 * 				Add this information as a new entry to your HashMap.
+
+	}
+
+	/*
+	 * Crate a HashMap of Integers for the keys and Strings for the values. Create a
+	 * GUI with three buttons. Button 1: Add Entry When this button is clicked, use
+	 * an input dialog to ask the user to enter an ID number. After an ID is
+	 * entered, use another input dialog to ask the user to enter a name. Add this
+	 * information as a new entry to your HashMap.
 	 * 
-	 * Button 2: Search by ID
-	 * 				When this button is clicked, use an input dialog to ask the user to enter an ID number.
-	 * 				If that ID exists, display that name to the user.
-	 * 				Otherwise, tell the user that that entry does not exist.
+	 * Button 2: Search by ID When this button is clicked, use an input dialog to
+	 * ask the user to enter an ID number. If that ID exists, display that name to
+	 * the user. Otherwise, tell the user that that entry does not exist.
 	 * 
-	 * Button 3: View List
-	 * 				When this button is clicked, display the entire list in a message dialog in the following format:
-	 * 				ID: 123  Name: Harry Howard
-	 * 				ID: 245  Name: Polly Powers
-	 * 				ID: 433  Name: Oliver Ortega
-	 * 				etc...
+	 * Button 3: View List When this button is clicked, display the entire list in a
+	 * message dialog in the following format: ID: 123 Name: Harry Howard ID: 245
+	 * Name: Polly Powers ID: 433 Name: Oliver Ortega etc...
 	 * 
-	 * When this is complete, add a fourth button to your window.
-	 * Button 4: Remove Entry
-	 * 				When this button is clicked, prompt the user to enter an ID using an input dialog.
-	 * 				If this ID exists in the HashMap, remove it. Otherwise, notify the user that the ID
-	 * 				is not in the list. 
+	 * When this is complete, add a fourth button to your window. Button 4: Remove
+	 * Entry When this button is clicked, prompt the user to enter an ID using an
+	 * input dialog. If this ID exists in the HashMap, remove it. Otherwise, notify
+	 * the user that the ID is not in the list.
 	 *
-	 * */
+	 */
 }
